6e9bbd93ca300b8fbd0f0619535defaa49ecc956
Lucy Ruiz <lucyruiz846@gmail.com>
2022-11-07 20:41:39 -0800

    Created RelatedVideos component and test of text display for description

---
+++ frontend/src/pages/VideoDisplay/RelatedVideos.jsx
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+
+const RelatedVideos = (props) => {
+
+
+    return(
+        <h1> Test relatedvideos</h1>
+    )
+}
+
+export default RelatedVideos;
--- frontend/src/pages/VideoDisplay/VideoDisplay.js
+++ frontend/src/pages/VideoDisplay/VideoDisplay.js
@@ -1,16 +1,27 @@
 import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
+import RelatedVideos from './RelatedVideos';
 
 const VideoDisplay = () => {
-
+    // const [selectedVideoId, setSelectedVideoId] = useState('');
     const {selectedVideo} = useParams()
+    // setSelectedVideoId(selectedVideo)
+
+    // async function getRelatedVideos(){
+    //     console.log('Obtaining related video to selectedVideos:', selectedVideoId)
+    //     return (
+  
+    //     )
+    // }
+
     return (
-        
+    <div>
         <iframe id="ytplayer" type="text/html" width="640" height="360"
 
-// I need a variable to change the key and display a video related to the input
   src={`https://www.youtube.com/embed/${selectedVideo}?autoplay=1&origin=http://example.com`}
   frameborder="0"></iframe>
+  <RelatedVideos/>
+   </div>
     )
 }
 
